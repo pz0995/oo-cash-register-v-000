bfaf23dbfb7d331ce216b91381b96c68cf107307
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-27 20:52:54 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,11 +23,10 @@ class CashRegister
         return "After the discount, the total comes to $#{@total}."
       else
         return "There is no discount to apply."
+      end
     end
-
     def void_last_transaction
       @total -= @transaction
 
     end
   end
-end
